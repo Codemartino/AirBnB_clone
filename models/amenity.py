43d5f4c44bef756003a5c1d275ff43379699f5e5
Valentine <akamigboifeanyi22@gmail.com>
2023-08-13 16:22:42 -0700

    amenity.py

---
+++ models/amenity.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""
+    Amenity modules
+"""
+from models.base_model import BaseModel
+
+
+class Amenity(BaseModel):
+    """
+        class Amenity inherit from base
+        Attribute:
+            name (str)
+    """
+    name = ""
+
+    def _init_(self, *args, **kwargs):
+        """
+            str
+        """
+        super()._init_(*args, **kwargs)
